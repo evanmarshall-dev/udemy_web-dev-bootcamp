49b34d1f4ce3d48647a1e72653ff5b2b31048caa
evanMarshallTheBay <evan.marshall@thebay.com>
2022-08-19 17:06:42 -0300

    Enforcing code styling and linting in the project.

--- notes/3_js/1_basics/4_truthy-falsey.js
+++ notes/3_js/1_basics/4_truthy-falsey.js
@@ -1,37 +1,37 @@
-const userInput = prompt('Enter something.');
+const userInput = prompt("Enter something.");
 // Do not need to compare anything as we are simply checking the userInput in the if statement.
 if (userInput) {
-  console.log('TRUTHY!');
+  console.log("TRUTHY!");
 } else {
-  console.log('FALSEY!');
+  console.log("FALSEY!");
 }
 // Below is the only values which are falsey.
 // Since 0 is one of the falsey values we can test with below example.
-if (0) {
-  console.log('TRUTHY');
-} else {
-  console.log('FALSEY');
-}
+// if (0) {
+//   console.log('TRUTHY');
+// } else {
+//   console.log('FALSEY');
+// }
 
 // Since null is one of the falsey values we can test with below example.
-if (null) {
-  console.log('TRUTHY');
-} else {
-  console.log('FALSEY');
-}
+// if (null) {
+//   console.log('TRUTHY');
+// } else {
+//   console.log('FALSEY');
+// }
 
 // Since NaN is one of the falsey values we can test with below example.
 if (NaN) {
-  console.log('TRUTHY');
+  console.log("TRUTHY");
 } else {
-  console.log('FALSEY');
+  console.log("FALSEY");
 }
 
 // Since undefined is one of the falsey values we can test with below example.
-if (undefined) {
-  console.log('TRUTHY');
-} else {
-  console.log('FALSEY');
-}
+// if (undefined) {
+//   console.log('TRUTHY');
+// } else {
+//   console.log('FALSEY');
+// }
 // Everything else is truthy.
 // The above strategies are used to determine if a value such as user input exists or not.
