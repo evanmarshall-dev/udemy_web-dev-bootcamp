5824184890899e80895e03de2c76b58c612f90ad
evanmarshall-dev <me@evanmarshall.dev>
2022-09-09 23:06:43 -0300

    Added my first function files to the function notes folder.

---
+++ notes/3_js/5_functions/2_function.js
@@ -0,0 +1,10 @@
+// STEP #1. Define the function.
+function funcName() {
+  // Inside here is where we put our code block which is executed once we RUN the function.
+  console.log("DO");
+  console.log("RE");
+  console.log("MI");
+}
+// STEP #2. Now we need to execute or run the function.
+funcName();
+// TIP. You should primarily define your functions before you use them or else you get into HOISTING.
