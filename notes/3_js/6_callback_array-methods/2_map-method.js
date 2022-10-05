f11a8a5340e689f8544b866317fc9ddcc959cb76
evanmarshall-dev <me@evanmarshall.dev>
2022-10-04 23:14:53 -0300

    Added map method lecture to callback and array methods.

---
+++ notes/3_js/6_callback_array-methods/2_map-method.js
@@ -0,0 +1,10 @@
+// Map Method takes a function and adds code to a new array.
+// EXAMPLE 1.
+console.log(`________________
+EXAMPLE 1`);
+const numArray = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+];
+const doubles = numArray.map(function (num) {
+  return num * 2;
+});
