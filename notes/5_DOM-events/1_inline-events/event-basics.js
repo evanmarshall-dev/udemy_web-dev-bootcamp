a6baa671be12ca23fa7c745113450abec3b87cf7
evan-marshall_bay <evan.marshall@thebay.com>
2023-04-08 18:00:50 -0300

    🎉 feat(event-basics.html): add HTML file for Event Basics This commit adds an HTML file for Event Basics. It includes two buttons, one with an inline event listener and another with an event listener added via JavaScript. The inline event listener is not recommended due to issues with quotes and redundancy. The second method is preferred as it allows for cleaner code and easier maintenance.
    📝 docs(event-basics.js): add notes on three ways to run code on click event
    This commit adds notes on three ways to run code on click event. The first way is using inline events, the second way is using the event property, and the third way is using addEventListener. The notes also include examples of how to use these methods and some important points to keep in mind when using them.

---
+++ notes/5_DOM-events/1_inline-events/event-basics.js
@@ -0,0 +1,26 @@
+// ? - EVENT BASICS
+// Three ways to run code on click event.
+// * #1. Inline Events.
+// * #2. Event Property.
+// Create variable to select the element.
+const btn = document.querySelector("#v2");
+// Add the onlick method with function.
+btn.onclick = () => {
+  console.log("You clicked me!");
+  console.log("I hope it worked.");
+};
+// Add separate function before event.
+function scream() {
+  console.log("AAAHHHHH!!!");
+  console.log("Stop touching me!");
+}
+// Then the onmouseenter event is set to the function.
+// The function scream is not being called by me. It is being set to the onmouseenter event. When the user performs the event is when the function is called.
+btn.onmouseenter = scream;
+
+// Even when you set an event inline the console assigns it to a function in the console.
+// Also, these events can be set to any HTML element, not just buttons.
+// You need to set the events to a function instead of simply setting the event to, let's say, a console.log() is actually running the function NOT setting it to a function so it will run right away.
+// btn.onclick = alert("This will run right away.");
+
+// * #3. addEventListener.
