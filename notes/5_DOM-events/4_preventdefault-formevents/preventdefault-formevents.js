8b8f06131f78e546217667a5da612819b94d4475
evan-marshall_bay <evan.marshall@thebay.com>
2023-05-21 12:25:50 -0300

    🎨 style(preventdefault-formevents): add cat name to unordered list 🐛 fix(preventdefault-formevents): reset form input after submission The form now adds the cat name to an unordered list instead of just logging it to the console. The form input is now reset to an empty string after submission.

--- notes/5_DOM-events/4_preventdefault-formevents/preventdefault-formevents.js
+++ notes/5_DOM-events/4_preventdefault-formevents/preventdefault-formevents.js
@@ -1,7 +1,20 @@
 // Now to prevent default behaviour of a form.
 const form = document.querySelector("#shelterForm");
+// Input has a few attributes available, but we will use .value. You can set the value equal to something or list what the value is.
+const input = document.querySelector("#catName");
+const list = document.querySelector("#cats");
 form.addEventListener("submit", function (e) {
-  // This is the code for preventing default behaviour.
+  // This is the code for preventing default behaviour so the form does not submit and go to new URL.
   e.preventDefault();
-  console.log("SUBMITTED THE FORM!!");
+  console.log(input.value);
+  // Add input.value to the li.
+  const catName = input.value;
+  // Create the li's.
+  const newLi = document.createElement("LI");
+  // Now add input.value to the empty li.
+  newLi.innerText = catName;
+  // Now we need to append the newly created li with value to the un-ordered list.
+  list.append(newLi);
+  // Reset the form input back to empty.
+  input.value = "";
 });
