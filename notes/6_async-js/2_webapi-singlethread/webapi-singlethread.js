fb2f688cd4c5b27ee481c43e913e291241f956d1
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-03 15:34:06 -0300

    📝 docs(webapi-singlethread.js): add explanation of how setTimeout works This commit adds a new file, webapi-singlethread.js, which demonstrates how JavaScript can only run one line of code at a time. The code logs three messages to the console, but the second message is delayed by 3 seconds using the setTimeout() function. The commit also includes an explanation of how setTimeout() works by passing the process off to the browser's WebAPI, which sets a timer for 3 seconds and then reminds JS to run the console.log.

---
+++ notes/6_async-js/2_webapi-singlethread/webapi-singlethread.js
@@ -0,0 +1,7 @@
+console.log("I printed first");
+setTimeout(() => {
+  console.log("I print after 3 seconds");
+}, 3000);
+console.log("I printed second");
+
+// JavaScript can only run one line of code at a time (single threaded). So the above code runs the first console.log then the third and finally the second one after 3 seconds. The code doesn't just sit there for 3 seconds without doing anything because JS hands this process off to the browser's WebAPI. The webAPI function is the setTimeout(). When JS sees this it then passes it off to the browser to set a timer for 3 seconds then remind JS to run the console.log.
