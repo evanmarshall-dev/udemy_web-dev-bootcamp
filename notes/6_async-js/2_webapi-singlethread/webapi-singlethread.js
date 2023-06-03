58c527b117daf0fc2513e9030959e1deb8eb892a
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-03 15:34:39 -0300

    📝 docs(webapi-singlethread.js): add explanation of what happens during the setTimeout() function call Add an explanation of what happens during the setTimeout() function call. The code runs the first console.log, then the third, and finally the second one after 3 seconds. JavaScript hands this process off to the browser's WebAPI, which sets a timer for 3 seconds and reminds JS to run the console.log. During this handoff and timer, other code can be executed.

--- notes/6_async-js/2_webapi-singlethread/webapi-singlethread.js
+++ notes/6_async-js/2_webapi-singlethread/webapi-singlethread.js
@@ -4,4 +4,4 @@ setTimeout(() => {
 }, 3000);
 console.log("I printed second");
 
-// JavaScript can only run one line of code at a time (single threaded). So the above code runs the first console.log then the third and finally the second one after 3 seconds. The code doesn't just sit there for 3 seconds without doing anything because JS hands this process off to the browser's WebAPI. The webAPI function is the setTimeout(). When JS sees this it then passes it off to the browser to set a timer for 3 seconds then remind JS to run the console.log.
+// JavaScript can only run one line of code at a time (single threaded). So the above code runs the first console.log then the third and finally the second one after 3 seconds. The code doesn't just sit there for 3 seconds without doing anything because JS hands this process off to the browser's WebAPI. The webAPI function is the setTimeout(). When JS sees this it then passes it off to the browser to set a timer for 3 seconds then remind JS to run the console.log. During this handoff and timer
