dfc57b908206ed8921b614b6864ec6bb3da89d5b
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-09 15:42:20 -0300

    🔥 chore(async-await.js): comment out unused login function and its invocation The login function and its invocation are commented out as they are not being used in the current implementation. This is a cleanup task to remove unused code.

--- notes/6_async-js/5_async-await/async-await.js
+++ notes/6_async-js/5_async-await/async-await.js
@@ -25,18 +25,18 @@
 //     console.log(err);
 //   });
 
-const login = async (username, password) => {
-  if (!username || !password) throw "Missing credentials";
-  if (password === "corgifeetarecute") return "Welcome!";
-  throw "Invalid password";
-};
+// ? const login = async (username, password) => {
+//   if (!username || !password) throw "Missing credentials";
+//   if (password === "corgifeetarecute") return "Welcome!";
+//   throw "Invalid password";
+// };
 
-login("ddwnklffwf", "corgifeetarecute")
-  .then((msg) => {
-    console.log("Logged in");
-    console.log(msg);
-  })
-  .catch((err) => {
-    console.log("Error!");
-    console.log(err);
-  });
+// login("ddwnklffwf", "corgifeetarecute")
+//   .then((msg) => {
+//     console.log("Logged in");
+//     console.log(msg);
+//   })
+//   .catch((err) => {
+//     console.log("Error!");
+//     console.log(err);
+//   });
