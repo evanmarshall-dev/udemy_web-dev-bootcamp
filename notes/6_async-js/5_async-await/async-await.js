9d8d10c5c40f8309ee8669da236cd4ba37fa1d7b
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-09 15:41:57 -0300

    🔒 chore(async-await.js): add login function ✨ feat(async-await.js): add login function to handle user authentication The login function takes in a username and password and checks if they are valid. If the credentials are missing, the function throws an error with the message "Missing credentials". If the password is correct, the function returns "Welcome!". If the password is incorrect, the function throws an error with the message "Invalid password". This function can be used to handle user authentication in an application.

--- notes/6_async-js/5_async-await/async-await.js
+++ notes/6_async-js/5_async-await/async-await.js
@@ -8,10 +8,35 @@
 
 // If something is returned in the async function the promise will be fulfilled based on that return.
 // The below function will return with the value "LAJFNFEK"
-const hello = async () => {
-  return "LAJFNFEK";
+// We reject a promise by throwing in an error inside an async function. This can be achieved by a syntax error or throw new Error("This is an error").
+// Therefore if the async function gives a value the promise succeeds and if it is an error the promise is rejected.
+// ? const hello = async () => {
+//   // Example error.
+//   throw new Error("Oopsie daisy... that is an error!");
+//   // return "LAJFNFEK";
+// };
+// // I can then chain on a .then.
+// hello()
+//   .then((data) => {
+//     console.log("Promise resolved with:", data);
+//   })
+//   .catch((err) => {
+//     console.log("Oh no, promise rejected!");
+//     console.log(err);
+//   });
+
+const login = async (username, password) => {
+  if (!username || !password) throw "Missing credentials";
+  if (password === "corgifeetarecute") return "Welcome!";
+  throw "Invalid password";
 };
-// I can then chain on a .then.
-hello().then((data) => {
-  console.log("Promise resolved with:", data);
-});
+
+login("ddwnklffwf", "corgifeetarecute")
+  .then((msg) => {
+    console.log("Logged in");
+    console.log(msg);
+  })
+  .catch((err) => {
+    console.log("Error!");
+    console.log(err);
+  });
