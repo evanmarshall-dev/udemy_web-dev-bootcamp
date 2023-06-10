c9fed6f4a7eba74e73a12834cd899fb5fc4be0e9
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-09 23:19:09 -0300

    🎨 style(async-await.html): add background color transition to body This commit adds a background color transition to the body of the HTML document. The transition is set to 0.5 seconds and will provide a smoother visual experience when the background color changes.
    🔀 merge(6_async-js/5_async-await): add async/await examples
    This commit adds examples of using async/await in JavaScript. The first example shows how to use await to pause the execution of code until a promise is resolved. The second example shows how to handle errors when using async/await by using try/catch.

--- notes/6_async-js/5_async-await/async-await.js
+++ notes/6_async-js/5_async-await/async-await.js
@@ -40,3 +40,85 @@
 //     console.log("Error!");
 //     console.log(err);
 //   });
+
+// * AWAIT
+// ? const rainbowColor = (color, delay) => {
+//   // ? return new Promise((resolve, reject) => {
+//   // Removed reject to pass linting.
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       document.body.style.backgroundColor = color;
+//       resolve();
+//     }, delay);
+//   });
+// };
+
+// ? rainbowColor("red", 1000)
+//   .then(() => rainbowColor("orange", 1000))
+//   .then(() => rainbowColor("yellow", 1000))
+//   .then(() => rainbowColor("green", 1000))
+//   .then(() => rainbowColor("blue", 1000))
+//   .then(() => rainbowColor("indigo", 1000))
+//   .then(() => rainbowColor("violet", 1000));
+
+// ? async function rainbow() {
+//   // Without await keyword only orange would happen because code is run at the same time. Things will just pause until the first promise (red color) is resolved in a second.
+//   // You can simply keep adding await.
+//   await rainbowColor("red", 1000);
+//   // The console.log will only execute after the first await.
+//   // console.log("Hi");
+//   await rainbowColor("orange", 1000);
+//   await rainbowColor("yellow", 1000);
+//   await rainbowColor("green", 1000);
+//   await rainbowColor("blue", 1000);
+//   await rainbowColor("indigo", 1000);
+//   await rainbowColor("violet", 1000);
+//   // Return to make a resolved promise.
+//   return "All done!!!!";
+// }
+
+// ? rainbow().then(() => {
+//   console.log("End of rainbow.");
+// });
+
+// OR
+
+// ? async function printRainbow() {
+//   // Common to store result in variable.
+//   const data1 = await rainbow();
+//   // Prints the return value of rainbow() to the console.
+//   console.log(data1);
+// }
+
+// ? printRainbow();
+
+// * Handling Async Errors
+
+const fakeRequestPromise = (url) => {
+  return new Promise((resolve, reject) => {
+    const delay = Math.floor(Math.random() * 4500) + 500;
+    setTimeout(() => {
+      if (delay > 4000) {
+        reject("Your fake request's connection timed out :(");
+      } else {
+        resolve(`Here is your fake data from ${url}`);
+      }
+    }, delay);
+  });
+};
+
+async function makeTwoRequests() {
+  // It is only stored in a variable when resolved. When error is thrown it is not a real error and problematic when code is run after i.e. console.log(data1) because the execution of code stops prior to more code.
+  // This issue can be resolved using try/catch.
+  try {
+    const data1 = await fakeRequestPromise("/page1");
+    console.log(data1);
+    const data2 = await fakeRequestPromise("/page2");
+    console.log(data2);
+  } catch (e) {
+    console.log("Caught an error!!");
+    console.log("Error is:", e);
+  }
+}
+
+makeTwoRequests();
