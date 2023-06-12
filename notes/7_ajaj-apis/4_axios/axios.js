996bbe593edf6bde1d9d70e5269e8caf384f97e3
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-12 17:07:53 -0300

    🎨 style(axios.html): add button and ul elements to the page The button and ul elements are added to the page to allow users to click and get new jokes. The ul element will be used to append the jokes to when the button is clicked.
    📝 docs(axios.js): add comments to explain headers and axios usage
    This commit adds comments to explain how to use headers with axios. It also adds a function that requests a new joke from an API and appends it to an unordered list element in the HTML document. The function uses axios to make a GET request to the API and passes in a headers object as a configuration object in the second argument of the axios.get() method. The headers object contains the Accept key with a value of "application/json". The function then returns the joke text from the API response. The addNewJoke() function is an event listener for a button click and calls the getDadJoke() function to get a new joke and appends it to the unordered list element.

--- notes/7_ajaj-apis/4_axios/axios.js
+++ notes/7_ajaj-apis/4_axios/axios.js
@@ -21,3 +21,44 @@ const getSWPerson = async (id) => {
 
 getSWPerson(5);
 getSWPerson(10);
+
+// * Headers and Axios.
+// Select the ul we will be appending jokes to.
+const jokes = document.querySelector("#jokes");
+// Select the button.
+const button = document.querySelector("button");
+
+const getDadJoke = async () => {
+  try {
+    // You can pass a second argument into axios.get() which contains config info for headers.
+    // Pass in the "headers" object as the config in the second argument.
+    // You can add to the end of the config with a comma if you need to add more headers or other config types.
+    const config = { headers: { Accept: "application/json" } };
+    const res = await axios.get("https://icanhazdadjoke.com/", config);
+    // This particular API requires us to set headers to Accept:application/json. Some APIs will require you to add /json to end of the endpoint and sometimes you need to add a query to the end of the endpoint.
+    // ? console.log(res);
+    // ? console.log(res.data.joke);
+
+    // getDadJoke function exists only to return the joke text, but also returns a Promise. Therefore we can make the above function addNewJoke() an async function which awaits getDadJoke() function.
+    return res.data.joke;
+  } catch (e) {
+    return "NO JOKES AVAILABLE! SORRY :(";
+  }
+};
+
+// Function will request a new joke
+const addNewJoke = async () => {
+  const jokeText = await getDadJoke();
+  console.log(jokeText);
+  // Create new li element.
+  const newLI = document.createElement("LI");
+  // Append the data for jokes from API or jokeText.
+  newLI.append(jokeText);
+  // Append new li to the ul with id of jokes.
+  jokes.append(newLI);
+};
+
+// Event listener for button click and run the addNewJoke() function.
+button.addEventListener("click", addNewJoke);
+
+getDadJoke();
