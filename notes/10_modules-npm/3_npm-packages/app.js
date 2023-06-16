963cc736e2170d140eb0e8b96ea374daabd3d38d
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-16 00:04:44 -0300

    📦 chore(package.json): add give-me-a-joke and colors packages as devDependencies ✨ feat(app.js): add give-me-a-joke and colors packages to app.js The give-me-a-joke and colors packages were added as devDependencies to the package.json file. The app.js file was updated to import the give-me-a-joke and colors packages. The give-me-a-joke package was used to retrieve a random dad joke and the colors package was used to add color to the output.

---
+++ notes/10_modules-npm/3_npm-packages/app.js
@@ -0,0 +1,10 @@
+const jokes = require("give-me-a-joke");
+// ? console.dir(jokes);
+const colors = require("colors");
+
+// Running ```node app.js`` in terminal shows that this package has four different methods that are exported from give-me-a-joke.
+// The below function is from their docs and you see that a callback function is used.
+jokes.getRandomDadJoke(function (joke) {
+  // We add .rainbow to the joke call in order to use the colors package
+  console.log(joke.rainbow);
+});
