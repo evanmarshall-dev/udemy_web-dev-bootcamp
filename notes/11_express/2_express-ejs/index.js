57d23f209b36093f2a7a9814542b4f292f5ff6c2
evan-marshall_bay <evan.marshall@thebay.com>
2023-06-19 17:06:05 -0300

    🎉 feat(index.js): add support for EJS templating engine This commit adds support for the EJS templating engine to the Express application. The app.set() method is used to set the view engine to EJS and the views directory to the /views folder. The app.get() method is used to define two routes, one for the home page and one for a random number page. The res.render() method is used to render the EJS templates and pass data to them.
    🎉 feat(ejs, package.json): add ejs dependency and create two new views
    The ejs dependency was added to the project to allow for the creation of dynamic HTML pages. Two new views were created, home.ejs and random.ejs. The home.ejs view contains a basic HTML structure with some EJS code to demonstrate how to use EJS. The random.ejs view is a simple HTML page that displays a random number passed in from the index.js file.

---
+++ notes/11_express/2_express-ejs/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Now to tell the app to use EJS.
+// app.set accepts to arguments (the key and the value). For EJS we will use the key of view engine and value of ejs.
+// We do not have to require the EJS package because the app.set() method and express will automatically use it.
+// Express expects templates to be in the views folder.
+app.set("view engine", "ejs");
+
+// * In order for express to work and see views directory, you need to run nodemon within the project folder. You cannot go out a few levels and run it from within the project folder.
+// You can fix this by setting the views folder. Use a node module called PATH (path.join) and app.set() method.
+// __dirname refers to whichever file you are in (index.js) is located.
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  // Now that we have home.ejs in views folder we can send back a file rather than a string. The method used for this is called render().
+  // ? res.send("Hello World");
+  // You do not need to render /views/home.ejs because it is in the views folder so express will look for it.
+  res.render("home");
+});
+
+// Define a random route.
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  // You can pass a second argument to render() method, which is an object.
+  // Num will be available in the template under the name rand. It is being past into the template via the object. You can name num key anything you want. It does not have to be rand.
+  // ? res.render("random", { rand: num });
+  // Or you can pass it in so key and value are the same so just num.
+  res.render("random", { num });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
