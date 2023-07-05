1f872e160ec7c7c295b16c42512ff9979b8557e4
evan-marshall_bay <evan.marshall@thebay.com>
2023-07-04 22:36:59 -0300

    🔧 fix(post-routes.js): use uuid package to generate unique IDs for comments The hardcoded IDs for comments have been replaced with unique IDs generated using the uuid package. This ensures that each comment has a unique identifier, improving the reliability and integrity of the application.
    🐛 fix(post-routes.js): add unique IDs for new comments using uuid package
    ✨ feat(post-routes.js): add comments/new route for viewing comment form
    The comments/new route is added to view the form for creating a new comment. The comments route now generates unique IDs for new comments using the uuid package. This ensures that each comment has a unique identifier, even after the original 4 comments.
    
    🐛 fix(post-routes.js): remove parseInt when finding comment by id
    🔥 feat(post-routes.js): add edit and update routes for comments
    The parseInt function is no longer needed when finding a comment by id because we are now using uuid, which does not provide an integer. The edit route is added to render a form for editing a comment, and the update route is added to handle the PATCH request for updating a comment. The update route finds the comment based on the id, updates the comment text based on the payload sent in the request body, and then redirects to the index or /comments page.
    
    📝 chore(views): add edit.ejs template for comments
    📝 chore(views): update index.ejs template for comments
    📝 chore(views): update show.ejs template for comments
    📝 chore(package.json): add method-override and uuid dependencies
    
    Added a new template file `edit.ejs` for comments. This template is used for editing comments and includes a form with a textarea for the comment content. The form uses the `POST` method and appends `?_method=PATCH` to the action URL to simulate a `PATCH` request.
    
    Updated the `index.ejs` template for comments to include a comment about not creating IDs for new comments and the need to fake a `PATCH` request.
    
    Updated the `show.ejs` template for comments to include a link to edit the comment.
    
    Added `method-override` and `uuid` dependencies to the `package.json` file. These dependencies are required

--- notes/11_express/7_REST/post-routes.js
+++ notes/11_express/7_REST/post-routes.js
@@ -1,12 +1,17 @@
 // We are going to use EJS to use forms in our HTML for a user to interact with COMMENTS.
 const express = require("express");
 const path = require("path");
+const methodOverride = require("method-override");
+// Below v4: uuid is destructuring uuid from the uuid package to get a new name of uuid.
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
 // The below req.body gets parsed due to the below two lines of code.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// _method is the string we are looking for in the query string.
+app.use(methodOverride("_method"));
 // Set the views directory
 app.set("views", path.join(__dirname, "views"));
 // Set the view engine to EJS
@@ -16,22 +21,26 @@ app.set("view engine", "ejs");
 // There is no database so we will also CREATE comments and push into this array.
 const comments = [
   {
-    id: 1,
+    // id: 1,
+    id: uuid(),
     username: "Todd",
     comment: "lol that is so funny!",
   },
   {
-    id: 2,
+    // id: 2,
+    id: uuid(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 3,
+    // id: 3,
+    id: uuid(),
     username: "Sk8erBoi",
     comment: "Plz delete your account, Todd",
   },
   {
-    id: 4,
+    // id: 4,
+    id: uuid(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
@@ -44,17 +53,18 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
 
-// For CREATE we will need to create two routes. One to view the form (NEW route is what it is usually called) and one to SUBMIT the form as a POST request, processed and added into comments array..
+// * For CREATE we will need to create two routes. One to view the form (NEW route is what it is usually called) and one to SUBMIT the form as a POST request, processed and added into comments array..
 app.get("/comments/new", (req, res) => {
   res.render("comments/new");
 });
 // * To read comments it uses a GET request to /comments and to create a comment it uses a POST request to /comments.
+// TODO: We are not making IDs for the new comments created after the original 4. It is tricky because we are faking the IDs in an array. We will need to use a package called uuid to create unique IDs for each comment.
 app.post("/comments", (req, res) => {
   // ? console.log(req.body);
   // destructure username and comment from req.body.
   const { username, comment } = req.body;
   // Add to the comments array by pushing in a new object which has username and comment.
-  comments.push({ username, comment });
+  comments.push({ username, comment, id: uuid() });
   // * After you submit data on the form you will see the username and comment when you go to /comments to run a new GET request or the READ route.
   // ? res.send("IT WORKED!");
   // Redirect to the READ route or index where all the comments are once you submit this post request.
@@ -70,9 +80,36 @@ app.get("/comments/:id", (req, res) => {
   // Find ID in request parameters.
   const { id } = req.params;
   // ID will be a string so we need to parseInt it.
-  const comment = comments.find((c) => c.id === parseInt(id));
+  // ! We do not want to call parseInt anymore because we are using uuid, which does not provide an integer.
+  // const comment = comments.find((c) => c.id === parseInt(id));
+  const comment = comments.find((c) => c.id === id);
   // Now to render a page.
   res.render("comments/show", { comment });
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === req.params.id);
+  // Render a form.
+  // Destructuring comment will give us access to comment object to prefill the form.
+  res.render("comments/edit", { comment });
+});
+
+// PUT request changes the whole thing, such as the whole comment, whereas a PATCH request changes a part of it, such as the username or comment.
+// * This is the UPDATE portion of CRUD.
+app.patch("/comments/:id", (req, res) => {
+  // res.send("PATCH!!!");
+  // If we want to update a comment we take the ID, find the comment we currently have and then update the comment based on some payload sent in the request body. Having a req.body is similar to a POST request.
+  // Taking ID from URL.
+  const { id } = req.params;
+  // Taking whatever was sent in the request body (payload).
+  const newCommentText = req.body.comment;
+  // Find comment based on ID.
+  const foundComment = comments.find((c) => c.id === id);
+  // Update comment property to be whatever was in the req.body.comment above.
+  foundComment.comment = newCommentText;
+  // We do not want respond with content from a patch route similar to a post route. We want to redirect to the index or /comments.
+  res.redirect("/comments");
 });
 
 app.listen(3000, () => {
